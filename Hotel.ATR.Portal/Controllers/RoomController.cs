aa0d0c05ebf8036acddb4e84f88411594a981cac
Khan-Slava <vaceslavhan22@gmail.com>
2024-02-01 09:48:18 +0600

    Добавьте файлы проекта.

---
+++ Hotel.ATR.Portal/Controllers/RoomController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.ATR.Portal.Controllers
+{
+    public class RoomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult RoomList() {
+            return View();
+        }
+        public IActionResult RoomDetail()
+        {
+            return View();
+        }
+    }
+}
